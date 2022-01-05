f89efb22cf166b03d2d5c3979d187a012bad3b0c
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-01-05 22:56:01 +0100

    Create checkHyperlinks.py
    Checking hyperlinks on a website for availability

---
+++ checkHyperlinks.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# checkHyperlinks.py
+
+# Purpose: Checking hyperlinks on a website for availability
+
+# Dependencies
+import requests
+from selenium import webdriver
+
+driver = webdriver.Firefox()
+url = 'http://webscrapingfordatascience.com'
+driver.get(url)
+
+driver.implicitly_wait(3)
+
+links = driver.find_elements_by_css_selector('a')
+
+workingLinks = []
+notWorkingLinks = []
+
+for link in links:
+    try:
+        statusCode = requests.head(link.get_attribute('href')).status_code
+        hyperlink = link.get_attribute('href')
+        if statusCode == 200:
+            print('Hyperlink is valid:\t', hyperlink)
+            workingLinks.append(hyperlink)
+        else:
+            print('Hyperlink is invalid:\t', hyperlink)
+            notWorkingLinks.append(notWorkingLinks)
+    except BaseException as err:
+        notWorkingLinks.append(err)
+        print(err)
+
+print('These hyperlinks work:')
+print(workingLinks)
+print()
+print("Those hyperlinks dont't work:")
+print(notWorkingLinks)
+
+driver.quit()
