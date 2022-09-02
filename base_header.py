d8b6722ac69d013cc3c9082d20dbc78523a32530
fenceMeshwire <94779778+fenceMeshwire@users.noreply.github.com>
2022-09-02 23:22:02 +0200

    Update and rename baseHeader.py to base_header.py

---
+++ base_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# base_header.py
+
+# Purpose: Change the HTML header to look like a regular request.
+
+# Dependency
+import requests
+
+url = 'http://webscrapingfordatascience.com/usercheck/'
+base_header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+
+base_request = requests.get(url, headers=base_header)
+status_code = base_request.status_code
+
+# Results
+print(status_code) # 200 OK
+print(base_request.text) # Welcome, normal user!
+print(base_request.request.headers) # {...}
